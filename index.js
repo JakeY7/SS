b3e89bc7bfc248768782584783d579756f0314a7
JakeY7 <44093576+JakeY7@users.noreply.github.com>
2018-11-25 23:37:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -21,3 +21,46 @@ render(
     </Provider>,
   document.getElementById('root')
 )
+//  <Drawer
+//                     className={classes.drawer}
+//                     variant="persistent"
+//                     anchor="right"
+//                     open={open}
+//                     classes={{
+//                         paper: classes.drawerPaper,
+//                     }}
+//                     style={{height:'100%'}}
+//                 >
+//                     <header style={{height:'30px',background:'red'}}>Session Chat</header>
+//                     <main style={{ height:`calc(100% - 70px)`,}}>
+
+//                         <div style={{background:'rgb(136, 232, 234)',height:'70px',border:'1px solid red',width:'100%',padding:'10px'}}>
+
+
+
+//                                 <List   dense={true} style={{background:'yellow',lineHeight:'15px',height:'50px',width:'100%',padding:'0'}}>
+//                                     <ListItem  style={{height:'15px',width:'100%',background:'red',marginBottom:"5px",padding:0}}>
+//                                     <ListItemText primary='Dwedge'/>
+//                                     <ListItemSecondaryAction style={{fontSize:'10px'}} dense={true}>
+//                                         May23,2017 1:20 PM
+
+//                                     </ListItemSecondaryAction>
+
+
+
+//                                 </ListItem>
+//                                     <div style={{overflow:'hidden',height:'30px',lineHeight:'15px',width:'100%',background:'yellow',paddingBottom:'5px',overflowWrap:'break-word'}}>
+
+//                                         hahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahaha
+
+//                                     </div>
+
+//                                 </List>
+
+//                         </div>
+
+//                     </main>
+//                     <footer style={{height:'40px',background:'red',padding:'10px 0 10px 10px',}}>
+//                         <input style={{width:'78%'}}/><button style={{marginLeft:'10px'}}>BU</button>
+//                   </footer>
+//                 </Drawer>
